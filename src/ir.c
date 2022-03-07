da7ee3bfaaf0b1cfa1cc08e20d342429590dbe44
garrisonhh <garrisonhh@pm.me>
2022-03-07 12:19:09 -0500

    more extensible ir representation
    swapping from three-address-code ir -> a higher level tagged union
    representation of IR. since I am compiling to C this makes a lot more
    sense, should make codegen super simple

--- src/ir.c
+++ src/ir.c
@@ -1,45 +1,33 @@
 #include "ir.h"
 
-char ITYPE_NAME[IT_COUNT][MAX_NAME_LEN];
-char IOP_NAME[IOP_COUNT][MAX_NAME_LEN];
+char ITYPE_NAME[ITYPE_COUNT][MAX_NAME_LEN];
+char IINST_NAME[II_COUNT][MAX_NAME_LEN];
+char IINST_TYPE_NAME[IIT_COUNT][MAX_NAME_LEN];
+
 #define X(A, B) B,
-const size_t IOP_NUM_OPERANDS[IOP_COUNT] = { IOP_TABLE };
+IInstType IINST_TYPE[II_COUNT] = { IINST_TABLE };
 #undef X
 
 void ir_init(void) {
     // table names
+#define X(A) #A,
+    char *itype_names[ITYPE_COUNT] = { ITYPE_TABLE };
+#undef X
 #define X(A, B) #A,
-    char *iop_names[IOP_COUNT] = { IOP_TABLE };
+    char *iinst_names[II_COUNT] = { IINST_TABLE };
 #undef X
 #define X(A) #A,
-    char *itype_names[IT_COUNT] = { ITYPE_TABLE };
+    char *iinst_type_names[IIT_COUNT] = { IINST_TYPE_TABLE };
 #undef X
 
-    names_to_lower(IOP_NAME, iop_names, IOP_COUNT);
-    names_to_lower(ITYPE_NAME, itype_names, IT_COUNT);
-
-#ifdef DEBUG
-    // ensure IOP_MAX_OPERANDS is accurate
-#define X(A, B) B,
-    size_t iop_num_opers[IOP_COUNT] = { IOP_TABLE };
-#undef X
-
-    size_t true_max = 0;
-
-    for (size_t i = 0; i < IOP_COUNT; ++i)
-        if (iop_num_opers[i] > true_max)
-            true_max = iop_num_opers[i];
-
-    if (true_max != IOP_MAX_OPERANDS)
-        fungus_panic("IOP_MAX_OPERANDS is %zu, should be %zu!",
-                     IOP_MAX_OPERANDS, true_max);
-#endif
+    names_to_lower(ITYPE_NAME, itype_names, ITYPE_COUNT);
+    names_to_lower(IINST_NAME, iinst_names, II_COUNT);
+    names_to_lower(IINST_TYPE_NAME, iinst_type_names, IIT_COUNT);
 }
 
 IRContext IRContext_new(void) {
     IRContext ctx = {
         .pool = Bump_new(),
-        .consts = Vec_new(),
         .funcs = Vec_new()
     };
 
@@ -55,7 +43,6 @@ void IRContext_del(IRContext *ctx) {
         IFuncEntry_del(ctx->funcs.data[i]);
 
     Vec_del(&ctx->funcs);
-    Vec_del(&ctx->consts);
     Bump_del(&ctx->pool);
 }
 
@@ -89,16 +76,7 @@ IFunc ir_add_func(IRContext *ctx, Word name, IType *params, size_t num_params) {
     return handle;
 }
 
-size_t ir_add_op(IRContext *ctx, IFunc func, IOpType type, size_t *opers,
-                   size_t num_opers) {
-#ifdef DEBUG
-    if (num_opers != IOP_NUM_OPERANDS[type]) {
-        fungus_panic("attempted to create a %s IOp with %zu operands "
-                     "(takes %zu)", IOP_NAME[type], num_opers,
-                     IOP_NUM_OPERANDS[type]);
-    }
-#endif
-
+size_t ir_add_op(IRContext *ctx, IFunc func, IOp op) {
     IFuncEntry *entry = IC_get_func(ctx, func);
 
     // allocate op
@@ -108,52 +86,62 @@ size_t ir_add_op(IRContext *ctx, IFunc func, IOpType type, size_t *opers,
     }
 
     // fill
-    IOp *op = &entry->ops[entry->len];
+    IOp *copy = &entry->ops[entry->len];
 
-    op->type = type;
-
-    for (size_t i = 0; i < num_opers; ++i)
-        op->operands[i] = opers[i];
+    *copy = op;
 
     return entry->num_params + entry->len++;
-}
-
-size_t ir_add_const(IRContext *ctx, IConst *iconst) {
-    IConst *copy = IC_alloc(ctx, sizeof(*copy));
-    size_t idx = ctx->consts.len;
-
-    *copy = *iconst;
-
-    Vec_push(&ctx->consts, copy);
-
-    return idx;
 }
 
 void ir_gen(Fungus *fun, IRContext *ctx, Expr *ast) {
     fungus_panic("TODO ir_gen");
 }
 
-static void IConst_dump(IConst *iconst) {
-    switch (iconst->type) {
-    case IT_BOOL: printf("%s", iconst->bool_ ? "true" : "false"); break;
-    case IT_I64: printf("%Ld", iconst->int_); break;
-    case IT_F64: printf("%Lf", iconst->float_); break;
-    case IT_COUNT: break;
+static void IOp_dump(IRContext *ctx, IOp *op) {
+    printf("%s ", IINST_NAME[op->inst]);
+
+    switch (IINST_TYPE[op->inst]) {
+    case IIT_NIL:
+    case IIT_COUNT:
+        break;
+    case IIT_CONST: {
+        IConst *iconst = &op->iconst;
+
+        printf("%s ", ITYPE_NAME[iconst->type]);
+
+        switch (iconst->type) {
+        case ITYPE_BOOL: printf("`%s`", iconst->bool_ ? "true" : "false"); break;
+        case ITYPE_I64: printf("`%Ld`", iconst->int_); break;
+        case ITYPE_F64: printf("`%Lf`", iconst->float_); break;
+        case ITYPE_COUNT: break;
+        }
+        break;
+    }
+    case IIT_UNARY:
+        printf("%zu", op->unary.a);
+        break;
+    case IIT_BINARY:
+        printf("%zu %zu", op->binary.a, op->binary.b);
+        break;
+    case IIT_CALL: {
+        IFuncEntry *entry = IC_get_func(ctx, op->call.func);
+
+        printf("%.*s(", (int)entry->name->len, entry->name->str);
+
+        for (size_t i = 0; i < op->call.num_params; ++i) {
+            if (i) printf(", ");
+            printf("%zu", op->call.params[i]);
+        }
+
+        printf(")");
+        break;
+    }
     }
 
     puts("");
 }
 
-static void IOp_dump(IOp *op) {
-    printf("%7s ", IOP_NAME[op->type]);
-
-    for (size_t i = 0; i < IOP_NUM_OPERANDS[op->type]; ++i)
-        printf("%4zu", op->operands[i]);
-
-    puts("");
-}
-
-static void IFuncEntry_dump(IFuncEntry *entry) {
+static void IFuncEntry_dump(IRContext *ctx, IFuncEntry *entry) {
     printf("%.*s(", (int)entry->name->len, entry->name->str);
 
     for (size_t i = 0; i < entry->num_params; ++i) {
@@ -164,8 +152,8 @@ static void IFuncEntry_dump(IFuncEntry *entry) {
     printf("):\n");
 
     for (size_t i = 0; i < entry->len; ++i) {
-        printf("%3s%4zu ", "", i + entry->num_params);
-        IOp_dump(&entry->ops[i]);
+        printf("%4s$%zu = ", "", i + entry->num_params);
+        IOp_dump(ctx, &entry->ops[i]);
     }
 }
 
@@ -174,18 +162,8 @@ void ir_dump(IRContext *ctx) {
     puts("IR:");
     term_format(TERM_RESET);
 
-    puts("consts:");
-
-    for (size_t i = 0; i < ctx->consts.len; ++i) {
-        printf("%3s%4zu ", "", i);
-
-        IConst_dump(ctx->consts.data[i]);
-    }
-
-    puts("");
-
     for (size_t i = 0; i < ctx->funcs.len; ++i)
-        IFuncEntry_dump(ctx->funcs.data[i]);
+        IFuncEntry_dump(ctx, ctx->funcs.data[i]);
 
     puts("");
 }
@@ -194,12 +172,15 @@ void ir_dump(IRContext *ctx) {
 void ir_test(Fungus *fun, IRContext *ctx) {
     IFunc main = ir_add_func(ctx, WORD("main"), NULL, 0);
 
-    IConst zero = (IConst){ .type = IT_I64, .int_ = 0 };
+    size_t v0 = ir_add_op(ctx, main, (IOp){
+        .inst = II_CONST,
+        .iconst = { ITYPE_I64, .int_ = 0 },
+    });
 
-    size_t c0 = ir_add_const(ctx, &zero);
-
-    size_t v0 = ir_add_op(ctx, main, IOP_CONST, &c0, 1);
-    size_t v1 = ir_add_op(ctx, main, IOP_RET, &v0, 1);
+    size_t v1 = ir_add_op(ctx, main, (IOp){
+        .inst = II_RET,
+        .unary = { v0 }
+    });
 
     ir_dump(ctx);
 }
--- src/ir.h
+++ src/ir.h
@@ -4,17 +4,37 @@
 #include "fungus.h"
 
 /*
- * ir contains only sparse type info; types can be extracted from c
+ * ir uses a generally generic representation, types of any given instruction
+ * must be inferred by semantic analysis + codegen via the parameters of an IOp
  */
 
+
+/*
+ * TODO representing algebraic types through IType:
+ *
+ * require accounting for two things, parameterizing and sum vs. product
+ * - parameterizing is `naming` typed data
+ * - algebraic types create structured data
+ * -
+ *
+ * examples
+ * - unparameterized
+ *   - sum -> raw union
+ *   - prod -> tuple, ptr
+ * - parameterized
+ *   - sum -> enum, tagged union
+ *   - prod -> struct
+ */
 #define ITYPE_TABLE\
     X(BOOL)\
     X(I64)\
     X(F64)\
 
-#define X(A) IT_##A,
-typedef enum IType { ITYPE_TABLE IT_COUNT } IType;
+#define X(A) ITYPE_##A,
+typedef enum IType { ITYPE_TABLE ITYPE_COUNT } IType;
 #undef X
+
+extern char ITYPE_NAME[ITYPE_COUNT][MAX_NAME_LEN];
 
 typedef struct IConst {
     IType type;
@@ -25,38 +45,54 @@ typedef struct IConst {
     };
 } IConst;
 
-extern char ITYPE_NAME[IT_COUNT][MAX_NAME_LEN];
+typedef struct IFuncHandle { unsigned id; } IFunc;
 
-/*
- * descriptions of not perfectly clear IOPs:
- * const - gives a local name to an IConst in ctx->constants
- */
-
-// table of (iop, num operands)
-#define IOP_TABLE\
+// table of (iinst, type)
+#define IINST_TABLE\
     /* special */\
-    X(NOP,      0)\
-    X(RET,      1)\
-    X(CONST,    1)\
+    X(NOP,      IIT_NIL)\
+    X(RET,      IIT_UNARY)\
+    X(CONST,    IIT_CONST)\
+    X(CALL,     IIT_CALL)\
     /* math */\
-    X(ADD,      2)\
-    X(SUB,      2)\
-    X(MUL,      2)\
-    X(DIV,      2)\
-    X(MOD,      2)\
+    X(ADD,      IIT_BINARY)\
+    X(SUB,      IIT_BINARY)\
+    X(MUL,      IIT_BINARY)\
+    X(DIV,      IIT_BINARY)\
+    X(MOD,      IIT_BINARY)\
 
-#define IOP_MAX_OPERANDS 2
+#define IINST_TYPE_TABLE\
+    X(NIL)\
+    X(CONST)\
+    X(UNARY)\
+    X(BINARY)\
+    X(CALL)\
 
-#define X(A, B) IOP_##A,
-typedef enum IOpType { IOP_TABLE IOP_COUNT } IOpType;
+#define X(A, B) II_##A,
+typedef enum IInstruction { IINST_TABLE II_COUNT } IInst;
+#undef X
+#define X(A) IIT_##A,
+typedef enum IInstructionType { IINST_TYPE_TABLE IIT_COUNT } IInstType;
 #undef X
 
-extern char IOP_NAME[IOP_COUNT][MAX_NAME_LEN];
-extern const size_t IOP_NUM_OPERANDS[IOP_COUNT];
+extern char IINST_NAME[II_COUNT][MAX_NAME_LEN];
+extern IInstType IINST_TYPE[II_COUNT];
+extern char IINST_TYPE_NAME[IIT_COUNT][MAX_NAME_LEN];
 
-typedef struct IOp {
-    IOpType type;
-    size_t operands[IOP_MAX_OPERANDS]; // operand addresses
+typedef struct IOpUnary { size_t a; } IOpUnary;
+
+typedef struct IOperation {
+    IInst inst;
+    union {
+        IConst iconst;
+        struct { size_t a; } unary;
+        struct { size_t a, b; } binary;
+        struct {
+            IFunc func;
+            size_t *params;
+            size_t num_params;
+        } call;
+    };
 } IOp;
 
 /*
@@ -70,12 +106,9 @@ typedef struct IFuncEntry {
     size_t num_params;
 } IFuncEntry;
 
-typedef struct IFuncHandle { unsigned id; } IFunc;
-
 typedef struct IRContext {
     Bump pool;
 
-    Vec consts; // Vec<IConst *>
     Vec funcs; // Vec<IFuncEntry *>
 } IRContext;
 
@@ -88,9 +121,7 @@ IRContext IRContext_new(void);
 void IRContext_del(IRContext *);
 
 IFunc ir_add_func(IRContext *, Word name, IType *params, size_t num_params);
-size_t ir_add_op(IRContext *, IFunc, IOpType type, size_t *opers,
-                 size_t num_opers);
-size_t ir_add_const(IRContext *, IConst *); // copies constant to ctx
+size_t ir_add_op(IRContext *, IFunc func, IOp op); // copies op
 
 void ir_gen(Fungus *fun, IRContext *ctx, Expr *ast);
 void ir_dump(IRContext *ctx);
